6d9fb7ce1a81f1a726460d978fe53286785af678
isaac844966 <isaackeyz55@gmail.com>
2025-04-03 18:28:09 +0100

    react native template

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-2xl">SignIn</Text>
+    </SafeAreaView>
+  );
+};
+export default SignIn;
